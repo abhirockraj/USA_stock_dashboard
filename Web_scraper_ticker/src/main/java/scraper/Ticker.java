0828c221d4a4730943814892282c420ad5205fc3
abhirockraj <abhinavkool19@gmail.com>
2021-10-24 14:40:08 +0530

    completed USA Stock dashboard

--- Web_scraper_ticker/src/main/java/scraper/Ticker.java
+++ Web_scraper_ticker/src/main/java/scraper/Ticker.java
@@ -5,42 +5,60 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.ui.LoadableComponent;
 
 public class Ticker {
+
     @FindBy(xpath = "//tr//td[1]//a")
     private List<WebElement> stock_ticker;
     @FindBy(xpath = "//select[contains(@name,'perpage')]")
     private WebElement perpage;
     @FindBy(xpath = "//option[contains(@value,'10000')]")
     private WebElement select_10k;
+    @FindBy(xpath = "//tr//td[2]")
+    private  List<WebElement> company_name;
 
     @Test
     public void ticker_stock() {
         System.setProperty("webdriver.chrome.driver", "D:\\Finacial Topology\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//        PageFactory.initElements(driver, this);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         String URL = "https://stockanalysis.com/stocks/";
         driver.get(URL);
         driver.findElement(By.xpath("//select[contains(@name,'perpage')]")).click();
+//        perpage.click();
         driver.findElement(By.xpath("//option[contains(@value,'10000')]")).click();
+//        select_10k.click();
         List<WebElement> list = driver.findElements(By.xpath("//tr//td[1]//a"));
-        String ticker = null;
+        String t,cn;
         List<String[]> data = new ArrayList<String[]>();
         for (int i=1;i<=list.size();i++){
-            ticker = driver.findElement(By.xpath("(//tr//td[1]//a)["+i+"]")).getText();
-            data.add(new String[]{ticker});
+            t = driver.findElement(By.xpath("(//tr//td[1]//a)["+i+"]")).getText();
+            cn = driver.findElement(By.xpath("(//tr//td[2])["+i+"]")).getText();
+            System.out.println(t+" "+cn);
+            data.add(new String[]{t,cn});
             writeToCsvAtOnce("D:\\Finacial Topology\\USA_stockTicker.csv",data);
         }
+//        String t,cn;
+//        for (int i = 0; i < stock_ticker.size(); ++i){
+//            t = stock_ticker.get(i).getText();
+//            cn = company_name.get(i).getText();
+//            System.out.println(t+" "+cn);
+//            data.add(new String[]{t,cn});
+//            writeToCsvAtOnce("D:\\Finacial Topology\\USA_stockTicker.csv",data);
+//        }
     driver.close();
     }
     public  void  writeToCsvAtOnce(String filepath, List<String[]> data){
---
+++ main.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+ticket_file= pd.read_csv('USA_stockTicker.csv',index_col=False)
+st.title("USA Stock Analysis and Prediction")
+ticket_file.columns =['Ticker', 'Company_Name']
+cn = st.selectbox("Select Ticker",ticket_file['Company_Name'])
+ticker = ticket_file[ticket_file['Company_Name'] == cn]['Ticker']
+t= ticker.to_string(index=False)
+st.write('TICKER = ',t)
+df = yf.Ticker(t)
+col1, col2 ,col3= st.columns(3)
+number = st.number_input("Number of days/months/years",min_value=1,max_value=365,step=1 )
+period1 = st.selectbox("Select days/months/years",('d','mo','y'),index=0)
+col = st.selectbox("Select Parameter",('Open','High','Low','Close'),index=0)
+
+def create_dataset(dataset, time_step=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-time_step-1):
+		a = dataset[i:(i+time_step), 0]   ###i=0, 0,1,2,3-----99   100 
+		dataX.append(a)
+		dataY.append(dataset[i + time_step, 0])
+	return np.array(dataX), np.array(dataY)
+
+
+if col1.button("Show Finacials "):
+    st.dataframe (df.financials,width= 50000,height=50000)
+if(col3.button("visulalise")):
+    hist = df.history(period=str(number)+period1)
+    plt.figure(figsize=(10,10))
+    plt.plot(hist[col], color = 'g', label=col)
+    st.pyplot()
+if(col2.button("Predict and compare")):
+    if(period1 != 'y'):
+        st.write("Please Choose Time Frame at least 1 year also be sensible !!!")
+    else:
+        model=Sequential()
+        model.add(LSTM(50,return_sequences=True,input_shape=(100,1)))
+        model.add(LSTM(50,return_sequences=True))
+        model.add(LSTM(50))
+        model.add(Dense(1))
+        model.compile(loss='mean_squared_error',optimizer='adam')
+        model.load_weights('./checkpoints/my_checkpoint')
+        scaler=MinMaxScaler(feature_range=(0,1))
+        hist = df.history(period=str(number)+period1)
+        df_x_a=scaler.fit_transform(np.array(hist[col]).reshape(-1,1))
+        time_step = 100
+        x,y = create_dataset(df_x_a, time_step)
+        x =x.reshape(x.shape[0],x.shape[1] , 1)
+        y_pred=model.predict(x)
+        look_back=100
+        # shift test predictions for plotting
+        testPredictPlot = np.empty_like(df_x_a)
+        testPredictPlot[:, :] = np.nan
+        testPredictPlot[look_back:len(y_pred)+look_back, :] = y_pred
+        # plot baseline and predictions
+        plt.plot(scaler.inverse_transform(df_x_a),color='b', label='Actual data')
+        plt.plot(scaler.inverse_transform(testPredictPlot),color='r', label='Predicted data')
+        plt.legend()
+        st.pyplot()
